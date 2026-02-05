becd3f64aa8cf5f283b9a061dd7aced30b4c2c00
simonsvale <s.risbjerg@stofanet.dk>
2026-02-05 14:03:14 +0100

    Added mandelbrot skeleton

---
+++ mandelbrot/mandelbrot.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+"""
+Mandelbrot Set Generator
+Author: Simon Risbjerg
+Course: NSC2026
+"""
+def f(x):
+    """
+    Example function .
+    Parameters
+    ----------
+    x : float
+    Input value
+    Returns
+    -------
+    float
+    Output value
+    """
+    # TODO : Implement the algorithm
+pass
+
+
+if __name__ == "__main__":
+    pass
