9a1ea454aa15784c95c43f09b611669fbc1a0250
simonsvale <s.risbjerg@stofanet.dk>
2026-02-05 15:20:01 +0100

    Added visualization to the mandelbrot set for 100x100 resolution.

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,26 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""
-Mandelbrot Set Generator
-Author: Simon Risbjerg
-Course: NSC2026
-"""
-def f(x):
-    """
-    Example function .
-    Parameters
-    ----------
-    x : float
-    Input value
-    Returns
-    -------
-    float
-    Output value
-    """
-    # TODO : Implement the algorithm
-pass
-
 
 def mandelbrot_point(c, max_iter):
 
@@ -47,28 +27,34 @@ def mandelbrot(x_interval, y_interval, N_x = 1024, N_y = 1024, max_iter = 100):
     grid_x = np.linspace(x_interval[0], x_interval[1], N_x)
     grid_y = np.linspace(y_interval[0], y_interval[1], N_y)
 
+    mandelbrot_grid = np.zeros((1024, 1024))
+
     # Go through all points in the defined region.
     for i in range(N_x):
         for j in range(N_y):
             x = grid_x[i]
             y = grid_y[j]
-            print(x, y)
 
             c = complex(x, y)
             iter_count = mandelbrot_point(c, max_iter)
-            print(iter_count)
+            mandelbrot_grid[j, i] = iter_count
     
-
-    pass
+    return mandelbrot_grid
 
 
 if __name__ == "__main__":
 
-    # Seed the program for reproducibility.
-    np.random.seed(0)
-
     # The definition of the regions in the x and y direction.
-    x_interval = [-2, 1]
+    x_interval = [-2.0, 1.0]
     y_interval = [-1.5, 1.5]
 
-    mandelbrot(x_interval, y_interval)
+    x_res = 100
+    y_res = 100
+    max_iter = 100
+
+    mandelbrot_grid = mandelbrot(x_interval, y_interval, x_res, y_res)
+
+    plt.imshow(mandelbrot_grid, cmap="viridis")
+    plt.xlim(0, x_res)
+    plt.ylim(0, y_res)
+    plt.show()
