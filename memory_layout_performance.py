180b07bb0a5353e7fe851676d2c87e9a6e0b0925
simonsvale <s.risbjerg@stofanet.dk>
2026-02-19 14:53:48 +0100

    Added a script to check memory layout performance, utilizing the cache efficiently we get ~8x speedup.

---
+++ memory_layout_performance.py
@@ -0,0 +1,21 @@
+import numpy as np
+import time
+
+if __name__ == "__main__":
+    N = 10000
+
+    A = np.random.rand(N, N)
+    # A = np.asfortranarray(A)
+
+    t_s = time.perf_counter()
+    for i in range(N): 
+        row_sum = np.sum(A[i, :])
+    t_e = time.perf_counter()
+    print(f"Row wise = {t_e - t_s}")
+
+    t_s = time.perf_counter()
+    for j in range(N): 
+        column_sum = np.sum(A[:, j])
+    t_e = time.perf_counter()
+    print(f"Column wise = {t_e - t_s}")
+
