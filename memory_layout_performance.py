866a5429d9bf0e731563252dcac18e133a148e96
simonsvale <s.risbjerg@stofanet.dk>
2026-02-19 15:17:58 +0100

    Changed dashes to underscores to be able to import functions from other files more easily.

--- memory_layout_performance.py
+++ memory_layout_performance.py
@@ -7,15 +7,16 @@ if __name__ == "__main__":
     A = np.random.rand(N, N)
     # A = np.asfortranarray(A)
 
-    t_s = time.perf_counter()
-    for i in range(N): 
-        row_sum = np.sum(A[i, :])
-    t_e = time.perf_counter()
-    print(f"Row wise = {t_e - t_s}")
+    for i in range(10):
+        t_s = time.perf_counter()
+        for i in range(N): 
+            row_sum = np.sum(A[i, :])
+        t_e = time.perf_counter()
+        print(f"Row wise = {t_e - t_s}")
 
-    t_s = time.perf_counter()
-    for j in range(N): 
-        column_sum = np.sum(A[:, j])
-    t_e = time.perf_counter()
-    print(f"Column wise = {t_e - t_s}")
+        t_s = time.perf_counter()
+        for j in range(N): 
+            column_sum = np.sum(A[:, j])
+        t_e = time.perf_counter()
+        print(f"Column wise = {t_e - t_s}")
 
