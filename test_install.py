5e0a595238b25621912f24fb22c0f8b2618280cc
simonsvale <s.risbjerg@stofanet.dk>
2026-03-19 19:40:19 +0100

    Added python-graphviz to the mamba install comment

--- test_install.py
+++ test_install.py
@@ -1,6 +1,6 @@
 # mamba create -n nsc2026 python=3.11
 # mamba activate nsc2026
-# mamba install numpy matplotlib scipy numba pytest dask
+# mamba install numpy matplotlib scipy numba pytest dask python-graphviz
 # mamba install line_profiler
 
 # mamba env export > mandelbrot/environment.yml
@@ -14,11 +14,9 @@ import dask
 
 print("HELLO!")
 
-print(numpy.__version__)
-print(matplotlib.__version__)
-print(numba.__version__)
-print(scipy.__version__)
-print(pytest.__version__)
-print(dask.__version__)
-
-
+print("NumPy:", numpy.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Numba:", numba.__version__)
+print("Scipy:", scipy.__version__)
+print("Pytest:", pytest.__version__)
+print("Dask:", dask.__version__)
