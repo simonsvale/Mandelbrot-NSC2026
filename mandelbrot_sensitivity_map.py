a7082bb0836c4f55d3883f2314de7075f06a4884
simonsvale <s.risbjerg@stofanet.dk>
2026-04-16 16:33:45 +0200

    Added an implementation for the sensitivity map.

---
+++ mandelbrot_sensitivity_map.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+
+def escape_count(C, max_iter):
+
+    z = np.zeros_like(C)
+    cnt = np.full(C.shape, max_iter, dtype=np.int32)
+    esc = np.zeros(C.shape, dtype=bool)
+
+    for iter in range(max_iter):
+        z[~esc] = z[~esc]**2 + C[~esc]
+
+        newly = ~esc & (z.real**2 + z.imag**2 > 4.0)
+
+        cnt[newly] = iter
+        esc[newly] = True
+    return cnt
+
+
+if __name__ == "__main__":
+
+    # Define the seahorse valley region.
+    x_interval = [-0.7530, -0.7490]
+    y_interval = [0.0990, 0.1030]
+
+    x_res = 1024
+    y_res = 1024
+    max_iter = 1000 # 1000 iterations instead of 100 because we are in the seahorse valley region.
+
+    x_values = np.linspace(x_interval[0], x_interval[1], x_res)
+    y_values = np.linspace(y_interval[0], y_interval[1], y_res)
+
+    C = (x_values[np.newaxis, :] + 1j * y_values[:, np.newaxis]).astype(np.complex128)
+
+    eps32 = float(np.finfo(np.float32).eps)
+    print(eps32)
+    delta = np.maximum(eps32 * np.abs(C), 1e-10)
+
+    # Calculate n(c)
+    n_base = escape_count(C, max_iter).astype(float)
+    n_perturb = escape_count(C + delta, max_iter).astype(float)
+
+    # Calculate ∆n.
+    dn = np.abs(n_base - n_perturb)
+
+    # Calculate κ(c).
+    kappa = np.where(n_base > 0, dn / (eps32 * n_base), np.nan)
+    cmap_k = plt.cm.hot.copy()
+    cmap_k.set_bad("0.25")
+    vmax = np.nanpercentile(kappa, 99.9)
+
+    plt.imshow(
+        kappa, 
+        cmap=cmap_k, 
+        origin="lower", 
+        extent=[-0.7530, -0.7490, 0.0990, 0.1030], 
+        norm=LogNorm(vmin=1, vmax=vmax)
+    )
+    plt.colorbar(label=f"Log scale (κ >= 1)")
+    plt.title(f"Condition number approx κ(c) = ∆n / (ε32 * n(c))")
+    plt.show()
