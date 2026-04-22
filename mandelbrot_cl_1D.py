9bef79bba866b7b364d5e415254fff57fd92413c
simonsvale <s.risbjerg@stofanet.dk>
2026-04-22 14:15:54 +0200

    Optimized the 1D implementation even further by encorporating better kernel creation from my 2D implementation.

--- mandelbrot_cl_1D.py
+++ mandelbrot_cl_1D.py
@@ -7,7 +7,7 @@ import time, statistics
 def get_mandelbrot_program(ctx: cl.Context) -> cl.Program:
 
     program = cl.Program(ctx, """
-    __kernel void mandelbrotPixel(__global float *cReal, __global float *cImag, __global int *grid, const int MaximumIterations, const int N) {
+    __kernel void mandelbrotPixel32(__global float *cReal, __global float *cImag, __global int *grid, const int MaximumIterations, const int N) {
 
         // Get id.            
         int idx = get_global_id(0);
@@ -18,9 +18,9 @@ def get_mandelbrot_program(ctx: cl.Context) -> cl.Program:
         grid[idx] = MaximumIterations;
         
         // Create variables.
-        float zReal = 0.0f;
-        float zImag = 0.0f;
-        float zImagNew = 0.0f;
+        float zReal = 0.0;
+        float zImag = 0.0;
+        float zImagNew = 0.0;
 
         for (int iter = 0; iter <= MaximumIterations; iter++) {
                         
@@ -30,6 +30,35 @@ def get_mandelbrot_program(ctx: cl.Context) -> cl.Program:
 
             // Check if we escape.
             if (((zReal*zReal) + (zImag*zImag)) > 4.0f) {      
+                grid[idx] = iter;
+                return;
+            }
+        }
+    }
+                         
+    __kernel void mandelbrotPixel64(__global float *cReal, __global float *cImag, __global int *grid, const int MaximumIterations, const int N) {
+
+        // Get id.            
+        int idx = get_global_id(0);
+        int cr_idx = idx % N;
+        int ci_idx = idx / N;
+
+        // Pre allocate max_iter iterations.
+        grid[idx] = MaximumIterations;
+        
+        // Create variables.
+        double zReal = 0.0;
+        double zImag = 0.0;
+        double zImagNew = 0.0;
+
+        for (int iter = 0; iter <= MaximumIterations; iter++) {
+                        
+            zImagNew = (2.0 * zReal * zImag) + cImag[ci_idx];
+            zReal = (zReal*zReal) - (zImag*zImag) + cReal[cr_idx];
+            zImag = zImagNew;
+
+            // Check if we escape.
+            if (((zReal*zReal) + (zImag*zImag)) > 4.0) {      
                 grid[idx] = iter;
                 return;
             }
@@ -59,20 +88,26 @@ if __name__ == "__main__":
     # Create CL program.
     program = get_mandelbrot_program(context)
     
-    # Setup buffers.
-    N = 8192
-    work_size = (N * N)
-
+    # Setup Mandelbrot variables.
+    N = np.int32(8192)
     max_iter = np.int32(100)
+    host_grid = np.zeros(N*N, dtype=np.int32)
 
-    host_grid = np.zeros(work_size, dtype=np.int32)
+    float_type = np.float32
+
+    if float_type == np.float32:
+        implementation_type = "mandelbrotPixel32"
+    elif float_type == np.float64:
+        implementation_type = "mandelbrotPixel64"
+    else:
+        raise NotImplemented("Only float32 and float64 (double) has been implemented!")
 
     # Create linspace.
     x_interval: tuple[float, float] = (-2.0, 1.0)
     y_interval: tuple[float, float] = (-1.5, 1.5)
 
-    host_c_real: np.ndarray = np.linspace(x_interval[0], x_interval[1], N, dtype=np.float32)
-    host_c_imag: np.ndarray = np.linspace(y_interval[0], y_interval[1], N, dtype=np.float32)
+    host_c_real: np.ndarray = np.linspace(x_interval[0], x_interval[1], N, dtype=float_type)
+    host_c_imag: np.ndarray = np.linspace(y_interval[0], y_interval[1], N, dtype=float_type)
     
     # Grid containing the iterations.
     dev_grid = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, size=host_grid.nbytes)
@@ -80,15 +115,21 @@ if __name__ == "__main__":
     dev_c_imag = cl.Buffer(context, cl.mem_flags.HOST_WRITE_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=host_c_imag)
 
     # Create the kernel from the program.
-    kernel = program.mandelbrotPixel
-    #local_size = 8
+    kernel = cl.Kernel(program, implementation_type)
+    kernel.set_arg(0, dev_c_real)
+    kernel.set_arg(1, dev_c_imag)
+    kernel.set_arg(2, dev_grid)
+    kernel.set_arg(3, max_iter)
+    kernel.set_arg(4, N)
+
+    global_work_size = (int(N) * int(N),)
+    local_work_size = (128,) # 128 is the best.
 
     runs = 5
-
     time_list = []
     for _ in range(runs):
         t_s = time.perf_counter()
-        kernel(queue, host_grid.shape, None, dev_c_real, dev_c_imag, dev_grid, max_iter, np.int32(N))
+        cl.enqueue_nd_range_kernel(queue, kernel, global_work_size, local_work_size)
         queue.finish()
         t_e = time.perf_counter()
         time_list.append(t_e - t_s)
@@ -103,5 +144,5 @@ if __name__ == "__main__":
     grid = host_grid.reshape(N, N)
 
     # Display grid.
-    #plt.imshow(grid)
-    #plt.show()
+    plt.imshow(grid)
+    plt.show()
