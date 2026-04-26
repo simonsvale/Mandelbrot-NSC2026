f7b1503c310cbf5b03d7ed915f34d94b95e4cdb7
simonsvale <s.risbjerg@stofanet.dk>
2026-04-26 17:41:11 +0200

    Added a docstring and types for the function in the sensitivity map implementation.

--- mandelbrot_sensitivity_map.py
+++ mandelbrot_sensitivity_map.py
@@ -1,10 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+from numpy.typing import NDArray
 
+def escape_count(C: NDArray[np.complex128], max_iter: int) -> NDArray[np.int32]:
+    """
+    Calculates the escape count for the mandelbrot set.
 
-def escape_count(C, max_iter):
+    Parameters
+    -----------
+    C : NDArray[np.complex128]
+        An array of complex constant with the np.complex128 type.
 
+    max_iter : int 
+        The maximum number of iterations to calculate the mandelbrot point at the complex value c for.
+
+    Returns
+    --------
+    cnt : NDArray[np.int32]
+        An array with the mandelbrot escape counts.
+    """
     z = np.zeros_like(C)
     cnt = np.full(C.shape, max_iter, dtype=np.int32)
     esc = np.zeros(C.shape, dtype=bool)
@@ -22,8 +37,8 @@ def escape_count(C, max_iter):
 if __name__ == "__main__":
 
     # Define the seahorse valley region.
-    x_interval = [-0.7530, -0.7490]
-    y_interval = [0.0990, 0.1030]
+    x_interval = (-0.7530, -0.7490)
+    y_interval = (0.0990, 0.1030)
 
     x_res = 1024
     y_res = 1024
@@ -39,8 +54,8 @@ if __name__ == "__main__":
     delta = np.maximum(eps32 * np.abs(C), 1e-10)
 
     # Calculate n(c)
-    n_base = escape_count(C, max_iter).astype(float)
-    n_perturb = escape_count(C + delta, max_iter).astype(float)
+    n_base = escape_count(C, max_iter)
+    n_perturb = escape_count(C + delta, max_iter)
 
     # Calculate ∆n.
     dn = np.abs(n_base - n_perturb)
@@ -55,7 +70,7 @@ if __name__ == "__main__":
         kappa, 
         cmap=cmap_k, 
         origin="lower", 
-        extent=[-0.7530, -0.7490, 0.0990, 0.1030], 
+        extent=(-0.7530, -0.7490, 0.0990, 0.1030), 
         norm=LogNorm(vmin=1, vmax=vmax)
     )
     plt.colorbar(label=f"Log scale (κ >= 1)")
